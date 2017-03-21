6a1fbd55a02f8c972a0b9455a84b2c416fae16e7
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2017-03-21 09:21:43 +0530

    Modified.

---
+++ XamarinFormsLocalDb/Droid/LocalFileHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using XamarinFormsLocalDb.Droid;
+
+[assembly: Dependency(typeof(LocalFileHelper))]
+namespace XamarinFormsLocalDb.Droid
+{
+	public class LocalFileHelper : ILocalFileHelper
+	{
+		public string GetLocalFilePath(string filename)
+		{
+			string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+			if (!Directory.Exists(libFolder)) {
+				Directory.CreateDirectory(libFolder);
+			}
+
+			return Path.Combine(libFolder, filename);
+		}
+	}
+}
